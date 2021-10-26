7285c44bada13f1fa8a71cc61624093c097750c3
Anders Åstrand <anders.astrand@kry.se>
2021-10-26 15:29:52 +0200

    Day 1: Linting

--- day1/main.ts
+++ day1/main.ts
@@ -1,39 +1,39 @@
-import { strict as assert } from 'assert';
-import { readFileSync } from 'fs';
+import { strict as assert } from "assert";
+import { readFileSync } from "fs";
 
-let solve1 = function (file: string): number {
-    const nums: number[] = readFileSync(file, 'utf-8')
-        .split("\n")
-        .map(num => +num);
-    return nums.reduce((a, b) => a + b, 0);
-}
+const solve1 = function (file: string): number {
+  const nums: number[] = readFileSync(file, "utf-8")
+    .split("\n")
+    .map((num) => +num);
+  return nums.reduce((a, b) => a + b, 0);
+};
 
-let solve2 = function (file: string): number {
-    const nums: number[] = readFileSync(file, 'utf-8')
-        .split("\n")
-        .map(num => +num);
+const solve2 = function (file: string): number {
+  const nums: number[] = readFileSync(file, "utf-8")
+    .split("\n")
+    .map((num) => +num);
 
-    var seen: Set<number> = new Set<number>();
-    var sum: number = 0;
-    var found: boolean = false;
+  var seen: Set<number> = new Set<number>();
+  var sum: number = 0;
+  var found: boolean = false;
 
-    while (!found) {
-        for (let num of nums) {
-            seen.add(sum);
-            sum = sum + num;
+  while (!found) {
+    for (let num of nums) {
+      seen.add(sum);
+      sum = sum + num;
 
-            if (seen.has(sum)) {
-                found = true;
-                break;
-            }
-        }
+      if (seen.has(sum)) {
+        found = true;
+        break;
+      }
     }
+  }
 
-    return sum;
-}
+  return sum;
+};
 
-assert(solve1('./example.txt') === 3);
-console.log(solve1('./input.txt'));
+assert(solve1("./example.txt") === 3);
+console.log(solve1("./input.txt"));
 
-assert(solve2('./example2.txt') === 2);
-console.log(solve2('./input.txt'));
+assert(solve2("./example2.txt") === 2);
+console.log(solve2("./input.txt"));
