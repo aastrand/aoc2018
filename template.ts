846fb288f620cd8f7421a117181a68130690994b
Anders Åstrandd <anders@teknodromen.se>
2021-11-01 21:12:57 +0100

    Add linting

--- template.ts
+++ template.ts
@@ -1,26 +1,24 @@
-import { strict as assert } from "assert";
-import { readFileSync } from "fs";
+import { strict as assert } from 'assert';
+import { readFileSync } from 'fs';
 
-const parse = function (file: string): string[] {
-  return readFileSync(file, "utf-8")
-    .trim()
-    .split("\n");
-};
+const parse = (file: string): string[] => readFileSync(file, 'utf-8')
+  .trim()
+  .split('\n');
 
-const solve1 = function (file: string): number {
+const solve1 = (file: string): number => {
   const lines = parse(file);
 
   return 0;
 };
 
-const solve2 = function (file: string): number {
-  const lines = parse(file); 
+const solve2 = (file: string): number => {
+  const lines = parse(file);
 
   return 0;
 };
 
-//assert(solve1("./example.txt") === 3);
-console.log(solve1("./input.txt"));
+assert(solve1('./example.txt') === 3);
+console.log(solve1('./input.txt'));
 
-//assert(solve2("./example2.txt") === 2);
-console.log(solve2("./input.txt"));
+// assert(solve2("./example2.txt") === 2);
+console.log(solve2('./input.txt'));
